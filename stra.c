4165ca7c8fca14c6ec1d8607a0f94c400a5ec106
Kcptonls <cmoretti@cs.princeton.edu>
2021-03-02 21:07:22 -0500

    fixed things

--- stra.c
+++ stra.c
@@ -3,7 +3,7 @@
 /* Author: Kevin Chen                                            */
 /*--------------------------------------------------------------------*/
 
-#include <str.h>
+#include "str.h"
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
