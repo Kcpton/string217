e4c907a4f421bfeaf8cc2018a1b136784ae34802
Kcptonls <cmoretti@cs.princeton.edu>
2021-03-03 02:20:51 -0500

    wat

--- strp.c
+++ strp.c
@@ -51,15 +51,15 @@ char *Str_concat (char s1[], const char s2[]) {
 int Str_compare(const char s1[], const char s2[]) {
     char *p1 = (char*)s1;
     char *p2 = (char*)s2;
-    while (*p1 != '\0' && *p2 != 0) {
-        if(*p1 - *p2 != 0) {
-            return *p1 - *p2;
+    while (*p1 != '\0' && *p2 != '\0') {
+        if((int) (*p1 - *p2) != 0) {
+            return (int)(*p1 - *p2);
         }
         p1++;
         p2++;
     }
-    if(*p1 - *p2 != 0) {
-        return *p1 - *p2;
+    if((int) (*p1 - *p2) != 0) {
+        return (int) (*p1 - *p2);
     }
     return 0;
 }
@@ -76,10 +76,10 @@ char *Str_search(const char s1[], const char s2[]) {
     if (Str_getLength(s2) == 0) {
         return (char*) NULL;
     }
-    while(p1 != '\0') {
+    while(*p1 != '\0') {
         p2 = (char*)s2;
         p3 = p1;
-        while(*p3 == *p2 && *p3 != '\0' && p2 != '\0') {
+        while(*p3 == *p2 && *p3 != '\0' && *p2 != '\0') {
             p3++;
             p2++;
         }
